2ef4dc337443c24cf9cb13bf756daffe37d8cda5
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-04 00:45:16 +0000

    Add assignment and assignment submissions tables with fake data

--- migrations/assignment_submissions.sql
+++ migrations/assignment_submissions.sql
@@ -9,7 +9,7 @@ CREATE TABLE assignments (
 
 CREATE TABLE assignment_submissions (
   id SERIAL PRIMARY KEY NOT NULL,
-  assigment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   duration INTEGER,
   submission_date DATE
